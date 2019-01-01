d44e399a3c72c9971e39e758d57301aeb5e21568
zerofruit <14wnrkim@gmail.com>
2019-01-02 01:26:42 +0900

    Added custom user model

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django API exercise [![Build Status](https://travis-ci.org/zeroFruit/django-api-ex.svg?branch=master)](https://travis-ci.org/zeroFruit/django-api-ex)
+ 
--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# customize user model, core is the name of App and the user
+# is model in our App that we assign as the custom user model
+AUTH_USER_MODEL = 'core.User'
